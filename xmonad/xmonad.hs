e6b59f28a58b15f55df2850027c64d5841294f81
Cyril Cressent <cyril@cressent.org>
2013-05-10 23:17:09 +0100

    Add a fullscreen handler to xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,8 +1,11 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
 
+main :: IO ()
 main = xmonad =<< xmobar defaultConfig
         {
           modMask = mod4Mask
         , workspaces = ["1:main"] ++ map show [2..9]
+        , handleEventHook = fullscreenEventHook
         }
