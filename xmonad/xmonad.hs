4b8895f7e9776a180b33dad1c51316a4d70d3afb
Cyril Cressent <cyril@cressent.org>
2014-05-06 19:35:22 +0100

    Change workspaces names and make VLC float by default

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,11 +1,19 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageHelpers
+
+myWorkspaces = ["1:main"] ++ map show [2..8] ++ ["9:system"]
+
+myManageHook = composeAll
+    [ className =? "Vlc" --> doCenterFloat ]
 
 main :: IO ()
 main = xmonad =<< xmobar defaultConfig
         {
           modMask = mod4Mask
-        , workspaces = ["1:main"] ++ map show [2..9]
+        , terminal = "urxvt"
+        , workspaces = myWorkspaces
+        , manageHook = myManageHook
         , handleEventHook = fullscreenEventHook
         }
