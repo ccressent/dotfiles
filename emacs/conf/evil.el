63ea59ea225f7d44872d77c159d3bf12262639ca
Cyril Cressent <cyril@cressent.org>
2014-09-05 12:53:17 +0100

    Make the cursor red when in emacs mode

--- emacs/conf/evil.el
+++ emacs/conf/evil.el
@@ -1,4 +1,5 @@
 (setq evil-want-C-u-scroll t)
+(setq evil-emacs-state-cursor '("red" box))
 
 (evil-mode t)
 (global-evil-surround-mode t)
@@ -6,6 +7,5 @@
 (require 'key-chord)
 (key-chord-mode 1)
 (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
-;(define-key evil-insert-state-map (kbd "jk") 'evil-normal-state)
 
 (provide 'conf/evil)
