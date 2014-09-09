b797af2868ccc6aec9c58561538837e0c6ae25c7
Cyril Cressent <cyril@cressent.org>
2014-09-09 23:31:37 +0200

    Fix for red cursor in Emacs mode

--- emacs/conf/evil.el
+++ emacs/conf/evil.el
@@ -1,8 +1,14 @@
 (setq evil-want-C-u-scroll t)
-(setq evil-emacs-state-cursor '("red" box))
+(setq evil-emacs-state-cursor  '("red"  box))
+(setq evil-normal-state-cursor '("gray" box))
+(setq evil-visual-state-cursor '("gray" box))
+(setq evil-motion-state-cursor '("gray" box))
+(setq evil-insert-state-cursor '("gray" bar))
 
 (evil-mode t)
 (global-evil-surround-mode t)
+
+(evil-set-initial-state 'haskell-interactive-mode 'emacs)
 
 (require 'key-chord)
 (key-chord-mode 1)
