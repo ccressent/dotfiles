3760e644e712a34edc7e599ea98d9c4d5fe1065a
Cyril Cressent <cyril@cressent.org>
2018-09-11 17:12:38 -0700

    Change Doom's org-directory

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,4 +1,6 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
+(setq org-directory "~/notes/")
+
 ;; Number of lines of margin to keep at the top and bottom when moving around.
 (setq scroll-margin 20)
