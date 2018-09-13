8b5493ac164c9ae54ba1c1ac1d049c41914232d9
Cyril Cressent <cyril@cressent.org>
2018-09-13 11:19:24 -0700

    Add org-agenda options

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -12,9 +12,11 @@
      (file+headline "" "Notes")
      "* %u %?\n%i" :kill-buffer t)))
 
-(setq org-agenda-files (list org-directory))
 (setq org-refile-targets '((nil . (:maxlevel . 5))
                            (org-agenda-files . (:maxlevel . 5))))
+
+(setq org-agenda-files (list org-directory))
+(setq org-agenda-restore-windows-after-quit t)
 
 ;; Use full outline paths and do not try to complete a path in hierarchical
 ;; order: we use fuzzy matching to find the right target.
