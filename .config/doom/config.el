e9f198d6460c9e1061abe4f0933a7be13bb8bad8
Cyril Cressent <cyril@cressent.org>
2018-09-12 17:49:17 -0700

    Add org refiling and agenda settings

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -2,6 +2,17 @@
 
 (setq org-directory "~/notes/")
 
+(setq org-agenda-files (list org-directory))
+(setq org-refile-targets '((nil . (:maxlevel . 5))
+                           (org-agenda-files . (:maxlevel . 5))))
+
+;; Use full outline paths and do not try to complete a path in hierarchical
+;; order: we use fuzzy matching to find the right target.
+(setq org-refile-use-outline-path t
+      org-outline-path-complete-in-steps nil)
+
+(setq org-refile-allow-creating-parent-nodes 'confirm)
+
 ;; Number of lines of margin to keep at the top and bottom when moving around.
 (setq scroll-margin 20)
 
