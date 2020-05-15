65af6700e6937359abd52e2b695eb195d86b755a
Cyril Cressent <cyril@cressent.org>
2020-05-15 13:13:43 -0700

    Move everything org-mode into the literate config

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -9,61 +9,10 @@
 (setq ivy-re-builders-alist
     '((t . ivy--regex-ignore-order)))
 
-;; Variables to figure out
-;; org-tags-column
-;; org-tag-alist
-;; org-persistent-tag-alist and add @home @work?
-;; org-agenda-skip-deadline-prewarning-if-scheduled
-;; org-todo-keywords
-
-(setq org-directory (expand-file-name "~/notes/"))
-(setq org-default-notes-file (expand-file-name "refile.org" org-directory))
-
-(setq org-ellipsis " ⤵"
-      org-hide-emphasis-markers t)
-
-;; Display "entities", such as \pi and \alpha, as UTF-8 characters. Similarly,
-;; sub and superscript is displayed nicely, as long as the sub or superscripted
-;; text is enclosed in {}; e.g.: R_{t}, R^{2}.
-;;
-;; The actual buffer content remains ASCII, this is purely for display purposes.
-(setq org-pretty-entities t
-      org-use-sub-superscripts '{})
-
-(setq org-capture-templates
-  '(("t" "todo" entry
-     (file+headline "" "Tasks")
-     "* TODO %?\n%i" :kill-buffer t)
-
-    ("n" "note" entry
-     (file+headline "" "Notes")
-     "* %u %?\n%i" :kill-buffer t)))
-
-(setq org-refile-targets '((nil . (:maxlevel . 5))
-                           (org-agenda-files . (:maxlevel . 5))))
-
-;; When refiling, use the full org outline paths, prefixed by the file name, and
-;; do not try to complete a path in hierarchical order: we use fuzzy matching to
-;; find the right target.
-(setq org-refile-use-outline-path 'file
-      org-outline-path-complete-in-steps nil)
-
-;; Allow creation of new nodes when refiling, after confirmation.
-(setq org-refile-allow-creating-parent-nodes 'confirm)
-
-;; Add an advice to org-refile so that after a refile, all the org buffers get
-;; automatically saved.
-;;
-;; An alternative approach could have been to use org-after-refile-insert-hook,
-;; but while these functions are called after content is added to the refile
-;; target, they are called _before_ the content is removed from the old
-;; location, leaving the source buffer unsaved.
-(advice-add 'org-refile :after 'org-save-all-org-buffers)
-
-;; I'm experimenting with having all my org-agenda related settings in a
-;; literate configuration file. Let's tangle it whenever needed and load it.
+;; I'm experimenting with having all my org-mode and org-agenda related settings
+;; in a literate configuration file. Let's tangle it and load it.
 (require 'ob-tangle)
-(org-babel-load-file (expand-file-name "agenda.org" doom-private-dir))
+(org-babel-load-file (expand-file-name "orgmode.org" doom-private-dir))
 
 ;; Number of lines of margin to keep at the top and bottom when moving around.
 (setq scroll-margin 20)
@@ -84,6 +33,10 @@
 
   (:after evil-magit
     :when (featurep! :ui workspaces)
+    ;; Ideally I should change magit-mode-map to that my bindings are added to
+    ;; all the derived magit modes. I think the issue is that some of these
+    ;; magit mode specific maps already define a g prefix and override their
+    ;; parent map.
     :map magit-status-mode-map
       :nv "g t" #'+workspace:switch-next
       :nv "g T" #'+workspace:switch-previous)
