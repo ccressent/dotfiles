a4fe5e217b6f1fd57b6c40afdd48aa668a74e935
Cyril Cressent <cyril@cressent.org>
2020-09-25 18:16:50 -0700

    Add link completion for Sensu
    Use forge's data to provide interactive completion for issue/pr links
    for Sensu.

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -90,7 +90,8 @@
         +docsets)        ; ...or in Dash docsets locally
        lsp
       ;macos             ; MacOS-specific commands
-       magit             ;
+       (magit
+        +forge)
       ;make              ; run make tasks from Emacs
       ;pass              ; password manager for nerds
       ;pdf               ; pdf enhancements
