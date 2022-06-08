a86d9fea4202bfb41b53355d73737aa48510590e
Cyril Cressent <cyril@cressent.org>
2022-06-08 00:35:25 -0700

    Add +ccressent/org-copy-link autoload

--- .config/doom/autoload/ccressent.el
+++ .config/doom/autoload/ccressent.el
@@ -3,3 +3,14 @@
   "Open a file somewhere in ~/.dotfiles via a fuzzy filename search."
   (interactive)
   (doom-project-find-file (expand-file-name "~/.dotfiles")))
+
+(defun +ccressent/org-copy-link (&optional arg)
+  "Extract the URL from an org-mode link and add it to the kill ring."
+  (interactive "P")
+  (let* ((link (org-element-lineage (org-element-context) '(link) t))
+         (type (org-element-property :type link))
+         (path (org-element-property :path link))
+         (url  (concat type ":" path)))
+    (if (and type path)
+        (progn (kill-new url) (message "Copied: %s" url))
+      (message "Couldn't find any org link"))))
